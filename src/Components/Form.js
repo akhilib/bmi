0e87b193a3f11e3a139368946ab497629629c268
Akhil IB <ibakhil74@gmail.com>
2022-03-16 16:13:17 +0530

    first commit

---
+++ src/Components/Form.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+function Form({ getData }) {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [alert, setAlert] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //console.log(weight,height);
+    if (isNaN(weight) || isNaN(height)) {
+      setAlert(true);
+      
+    } else {
+      getData(weight, height);
+      setAlert(false);
+      setHeight("");
+      setWeight("");
+    }
+  };
+
+  return (
+    <div className="col-sm-5 shadow rounded px-5">
+      <h1 className="text-center pt-3 text-secondary h2 text-danger">BMI Calculator</h1>
+      <form onSubmit={onSubmit}>
+        <div className="row">
+          <div className="col col-sm-6">
+            <div className="my-3">
+              <label className="form-label h6">Weight(kg) :</label>
+              <input
+                type="text"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+          </div>
+          <div className="col col-sm-6">
+            <div className="my-3">
+              <label className="form-label h6">Height(m) :</label>
+              <input
+                type="text"
+                value={height}
+                onChange={(e) => setHeight(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+        <input type="submit" className="btn btn-primary my-3 " value="Get BMI"/>
+        </div>
+      </form>
+      {alert && (
+        <div className="alert alert-danger" role="alert">
+          Plz enter valid datas
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Form;
